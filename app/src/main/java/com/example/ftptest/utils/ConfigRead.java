460dc8c57a4f4737ca10da22d8b680fab2822680
Junxin Qiu <19302010010@fudan.edu.cn>
2021-11-28 21:54:16 +0800

    Update ConfigRead.java

--- app/src/main/java/com/example/ftptest/utils/ConfigRead.java
+++ app/src/main/java/com/example/ftptest/utils/ConfigRead.java
@@ -13,6 +13,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
+import android.util.Log;
+import android.os.Environment;
+
+
 public class ConfigRead {
     public static Logger configLogger = Logger.getLogger(ConfigRead.class);
     /*根目录的路径*/
@@ -28,50 +32,57 @@ public class ConfigRead {
     /*拥有权限的用户*/
     public static HashSet<String> adminUsers = new HashSet<String>();
 
-    //初始化根目录，权限用户，能够登录的用户信息
     public static void read(){
-        String path = System.getProperty("user.dir") + "/config/server.xml";
-
-        File file = new File(path);
-        SAXBuilder builder = new SAXBuilder();
-        try {
-            Document parse = builder.build(file);
-            Element root = parse.getRootElement();
-
-            //配置服务器的默认目录
-            rootDir = root.getChildText("rootDir");
-//            System.out.print("rootDir is:");
-//            System.out.println(rootDir);
-            configLogger.debug("rootDir is: "+rootDir);
-
-            //允许登录的用户
-            Element usersE = root.getChild("users");
-            List<Element> usersEC = usersE.getChildren();
-            String username = null;
-            String password = null;
-            System.out.println("\n所有用户的信息：");
-            for(Element user : usersEC) {
-                username = user.getChildText("username");
-                password = user.getChildText("password");
-//                System.out.println("用户名："+username);
-//                System.out.println("密码："+password);
-                configLogger.debug("用户名： "+username);
-                configLogger.debug("密码： "+password);
-                users.put(username,password);
-            }
-
-            /*
-            //拥有put权限和delete权限的用户
-            System.out.println("\n管理员用户：");
-            Element adminUsersE = root.getChild("adminUsers");
-            for(Element adminUserTemp: (List<Element>)adminUsersE.getChildren()) {
-                username = adminUserTemp.getText();
-                //System.out.println("用户名："+username);
-                adminUsers.add(username);
-            }   */
-        } catch (JDOMException | IOException e) {
-            e.printStackTrace();
-        }
+        users.put("admin","000000");
     }
+
+
+    //初始化根目录，权限用户，能够登录的用户信息
+//    public static void read(){
+////        String path = Environment.getExternalStorageState() + "app/res/config/server.xml";
+////        String path = System.getProperty("user.dir") + "app/res/config/server.xml";
+//        String path =
+//        Log.i("path:",path);
+//        File file = new File(path);
+//        SAXBuilder builder = new SAXBuilder();
+//        try {
+//            Document parse = builder.build(file);
+//            Element root = parse.getRootElement();
+//
+//            //配置服务器的默认目录
+//            rootDir = root.getChildText("rootDir");
+////            System.out.print("rootDir is:");
+////            System.out.println(rootDir);
+////            configLogger.debug("rootDir is: "+rootDir);
+//
+//            //允许登录的用户
+//            Element usersE = root.getChild("users");
+//            List<Element> usersEC = usersE.getChildren();
+//            String username = null;
+//            String password = null;
+//            System.out.println("\n所有用户的信息：");
+//            for(Element user : usersEC) {
+//                username = user.getChildText("username");
+//                password = user.getChildText("password");
+////                System.out.println("用户名："+username);
+////                System.out.println("密码："+password);
+////                configLogger.debug("用户名： "+username);
+////                configLogger.debug("密码： "+password);
+//                users.put(username,password);
+//            }
+//
+//            /*
+//            //拥有put权限和delete权限的用户
+//            System.out.println("\n管理员用户：");
+//            Element adminUsersE = root.getChild("adminUsers");
+//            for(Element adminUserTemp: (List<Element>)adminUsersE.getChildren()) {
+//                username = adminUserTemp.getText();
+//                //System.out.println("用户名："+username);
+//                adminUsers.add(username);
+//            }   */
+//        } catch (JDOMException | IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
